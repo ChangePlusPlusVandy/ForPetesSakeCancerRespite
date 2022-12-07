57a68dbe8b8a9d2b993815beb89e127fa06c62f0
Charlotte Barnes <charbarnz@gmail.com>
2022-12-06 20:54:50 -0600

    Added the endpoints.
    In postman: can create get, post, and delete requests
    
    here's what my body looks like in postman:
    
    {
       "title":"Weekend on vacation!",
       "body":"Had so so much fun. Blah blah blah.",
       "author":"Charlotte"
    }. Doing a post request with this for ocalhost:3000/api/newsletter/create_newsletter will create a post in my db.

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,34 @@
 import express from "express";
+import router_api from "./api";
+import * as Mongoose from "mongoose";
+import * as mongoDB from "mongodb";
 
 const router = express.Router();
+
+const uri = 'mongodb://localhost/Newsletter'; // can change 
+
+
+// // initialize connection
+// const client: mongoDB.MongoClient = new mongoDB.MongoClient("mongodb://localhost/Newsletter");
+// const account = async () => {
+// 	await client.connect()
+// 	.then(()=>console.log('successfully connected to db!'))
+// 	.catch(e=>console.log(e));;
+// };
+// account();
+
+Mongoose.connect(uri, (err: any) => {
+	if (err) {
+	  console.log(err.message);
+	} else {
+	  console.log("Successfully Connected!");
+	}
+  });
 
 router.get("/", async (req, res) => {
 	res.send("Hello World!");
 });
+
+router.use('/api', router_api);
 
 export default router;
