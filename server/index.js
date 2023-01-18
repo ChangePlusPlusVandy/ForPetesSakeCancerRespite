2f161d3b0e5b278f2debca44fe5a6cd9edfdca78
TDogaNazli <tutku.d.nazli@vanderbilt.edu>
2023-01-17 23:46:38 -0600

    messages are saved

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+const mongoose = require("mongoose")
+const Msg = require("./models/messages")
 const express = require("express")
 const app = express()
 const port = 4000
+const mongodb = "mongodb://localhost:27017";
 
 const http = require("http").Server(app)
 const cors = require("cors")
@@ -9,11 +12,29 @@ app.use(cors())
 
 const socketIO = require('socket.io')(http)
 
+mongoose.set('strictQuery', false);
+
+mongoose.connect(mongodb).then(() => {
+  console.log("connected to the database")
+})
+
 socketIO.on('connection', (socket) => {
     console.log(`${socket.id} user just connected!`);
-    socket.on("message", (data) => {
-        console.log(data);
-        socketIO.emit("message", data);
+    Msg.find().then(result => {
+      /*
+      result.forEach(message => {
+        console.log(message.msg);
+      })
+      */
+      socket.emit("output-messages", result);
+    })
+
+    socket.on("message", (msg) => {
+        const message = new Msg({msg});
+        message.save().then(() => {
+          console.log(msg);
+          socketIO.emit("message", msg);
+        })
       })
     socket.on("disconnect", () => {
       console.log(`${socket.id} user disconnected`);
