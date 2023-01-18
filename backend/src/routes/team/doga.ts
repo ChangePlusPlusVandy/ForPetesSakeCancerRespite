3f1ce827d12977203764c4c04414d68421f881f6
TDogaNazli <tutku.d.nazli@vanderbilt.edu>
2023-01-17 23:47:54 -0600

    chatApp updated to save messages

--- backend/src/routes/team/doga.ts
+++
@@ -1,7 +0,0 @@
-import teamModel from "../../models/User"
-
-const doga = (req, res) => {
-    res.json("Hi, it's Doga!")
-}
-
-export default doga;
--- frontend/App.js
+++
@@ -1,6 +0,0 @@
-import Main from "./src/Main";
-import Doga from "./src/doga"
-
-export default function App() {
-	return <Doga />;
-}
