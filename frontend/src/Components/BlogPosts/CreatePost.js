dee07523dd12c4f5383235dfb1ba4a6f79cdac74
Charlotte Barnes <charbarnz@gmail.com>
2023-01-25 16:36:01 -0600

    Added the frontned of create post, backend not working yet

--- frontend/src/Components/BlogPosts/CreatePost.js
+++ frontend/src/Components/BlogPosts/CreatePost.js
@@ -1,41 +1,109 @@
-// import * as React from "react";
-// import { Pressable, Alert, View, Text, TextInput } from "react-native";
-// import { Component } from "react";
-// import { StyleSheet } from "react-native";
-
-import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, TextInput, View, Button } from "react-native";
+import React, { useState, useEffect} from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
 import { useAuth } from "../../AuthContext";
-// import { Icon } from "@rneui/themed";
+import { IconButton, MD3Colors } from "react-native-paper";
 
 const CreatePost = () => {
   const { currentUserIn } = useAuth();
   const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = async (e) => {
+		e.preventDefault();
+		setIsLoading(true);
+		try {
+			await login(email, password);
+			history.push("/"); // Redirect to home page
+		} catch (error) {
+			setError(error.message);
+		}
+		setIsLoading(false);
+	};
 
   return (
-    <View style={styles.container}>
-      <View style = {{flexDirection: "row"}}>
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>Create post</Text>
         <TextInput
           style={styles.title_input}
-          placeholder="Title your post"
-          onChangeText={(newText) => setTitle(newText)}
-          defaultValue={title}
+          placeholder="Subject"
+          placeholderTextColor="#474C4D"
+          onChangeText={(e) => setTitle(e)}
         />
-        <Button
-          title = "Post your post">
-        </Button>
       </View>
-      <View style = {{height: 500, borderColor: "black"}}>
-        <TextInput placeholder="Here is where you would create a post" />
+      <View style={styles.container}>
+        <View
+          style={{
+            height: "58%",
+            width: "85%",
+          }}
+        >
+          <TextInput
+            multiline
+            allowFontScaling
+            enablesReturnKeyAutomatically="true"
+            placeholderTextColor="#474C4D"
+            style={styles.postInput}
+            textAlign={"center"}
+            placeholder="Write here..."
+            onChangeText={(e) => setBody(e)}
+          />
+        </View>
+        <View
+          style={{
+            justifyContent: "flex-end",
+            flexDirection: "row",
+            width: "100%",
+            height: "8%",
+          }}
+        >
+          <TouchableOpacity 
+          style={styles.postButton}
+          onPres = {handleSubmit}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: 400,
+                fontSize: 16,
+              }}
+            >
+              Publish
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
       <View style={styles.rectangle}>
-        <View>
-          {/* <Icon name="home" /> */}
-          <Button style = {styles.homeButton}></Button>
-          <Link to="/">Home</Link>
+        <View style = {styles.homeButtonContainer}>
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <IconButton
+              icon="home"
+              iconColor={"white"}
+              size={50}
+              onPress={() => console.log("Pressed")}
+            />
+          </Link>
+          <Link to="/profile" style={{ textDecoration: 'none' }}>
+            <IconButton
+              icon="account-circle"
+              iconColor={"white"}
+              size={50}
+              onPress={() => console.log("Pressed")}
+            />
+          </Link>
         </View>
       </View>
     </View>
@@ -46,73 +114,74 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+  postButton: {
+    backgroundColor: "#088DA9",
+    borderRadius: 15,
+    marginBottom: 20,
+    marginRight: "10%",
+    width: "25%",
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
-    flexDirection: "column", 
-  },
-  bottombar: {
-    position: "absolute",
-    width: 411,
-    height: 375,
-    left: 0,
-    top: 734,
-    backgroundColor: "#088DA9",
   },
   rectangle: {
-    width: 411,
-    position: "absolute",
+    width: "100%",
     bottom: 0,
-    height: 100,
+    position: "absolute",
+    height: "18%",
     backgroundColor: "#088DA9",
+    flexDirection: "row", 
+    justifyContent: "center"
   },
   title_input: {
-    height: "5%",
-    width: "50%",
-    // color: "#ffffff",
-    borderColor: "#5f6566",
-    // borderWidth: 1,
-    // alignItems: "left",
-    // justifyContent: "left",
+    height: "40%",
+    width: "55%",
+    alignItems: "left",
+    justifyContent: "left",
     borderRadius: 15,
-    marginTop: 10,
-    marginBottom: 10,
-    // marginBottom: 20,
+    padding: 10,
+    backgroundColor: "#d9d9d959",
+    borderColor: "#5f6566",
+    fontWeight: 700,
+    fontSize: 20,
+    color: "#474C4D",
   },
-
-  homeButton: {
-    /* Vector */
-    position: "absolute",
-    left: "15.09%", 
-    right: "77.62%", 
-    top: "92.83%", 
-    bottom: "3.52%",
-
-    background: "#F8F8F8",
+  homeButtonContainer: {
+    width: "85%",
+    margin: 10,
+    flexDirection: "row", 
+    justifyContent: "space-between"
+  },
+  postInput: {
+    height: "100%",
+    borderRadius: 15,
+    borderColor: "#5f6566",
+    width: "100%",
+    backgroundColor: "#d9d9d959",
+    padding: 15,
+    marginBottom: 20,
+    fontWeight: 500,
+    fontSize: 20,
+    color: "#474C4D",
+  },
+  titleContainer: {
+    height: "20%",
+    width: "100%",
+    alignItems: "left",
+    justifyContent: "space-evenly",
+    marginLeft: "8%",
+    marginTop: "20%",
+  },
+  titleText: {
+    fontWeight: "bold",
+    fontSize: 36,
+    color: "#088DA9",
+    fontStyle: "normal",
   },
 });
 
 export default CreatePost;
-
-// class CreatePost extends Component {
-//   render() {
-//     return (
-//     <View>
-//         Hello!
-//         <View style = {styles.bottombar} ></View>
-//     </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//     bottombar: {
-//         position: 'absolute',
-//         width: 411,
-//         height: 375,
-//         left: 0,
-//         top: 734,
-//         background: '#088DA9',
-//     }
-// })
-
-// export default CreatePost;
