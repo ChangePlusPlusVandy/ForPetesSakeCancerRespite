57ad9b0cd7229a018077645142df26a96c7bb9ed
Syed Ali <syedali@wadiyanEmbassy.localdomain>
2023-01-29 18:33:30 -0600

    removed backend

--- backend/src/routes/api/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-var router = express.Router();
-import router_newsletter from "./newsletter";
-
-router.use('/newsletter', router_newsletter);
-
-// router.use('/', require('./users'));
-
-export default router;
--- backend/src/routes/data/index.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const dataRoute = express.Router();
-
-dataRoute.get("/", (req, res) => {
-    res.send("Test Data Sent From Backend");
-});
-
-export default dataRoute;
