38ebf8834101d663fed9eff4570b4d9362b59ceb
HiroakiYo <lyang01354@gmail.com>
2023-02-16 17:28:39 -0600

    extracted BlogDisplay Component

--- frontend/src/Components/BlogPosts/FunctionalExplorer.js
+++ frontend/src/Components/BlogPosts/FunctionalExplorer.js
@@ -1,37 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet,Text,View, TouchableOpacity, FlatList, Image} from "react-native";
+import BlogDisplay from "./BlogDisplay"
 import Config from "../../Config";
 
-const NewsItem = (props) => {
-    return (
-         <View style={styles.newsItem}>
-            <View style={styles.newsHeader}>
-                <Image style={{flex:1, height:'100%', width:-1, borderRadius:'50%'}} source={require('../../../public/defaultProfile.png')}/>
-                <Text style={{flex:4, alignSelf:'center',marginLeft:'19px'}}>{props.author}</Text>
-                <Text style={{flex:4, alignSelf:'center'}}>Posted:1/11/202</Text>
-            </View>
-            <View style={styles.newsTitle}>
-                <Text style={{fontWeight:'bold', fontSize:'26px'}}>{props.title}</Text>
-            </View>
-            <TouchableOpacity style={{alignSelf:'stretch', flex:1, alignItems:'center',padding:'5px'}}>
-                <Image style={{flex:1, height:'100%', width:-1, aspectRatio:1}} source={require('../../../public/newsletter/ViewMore.png')}></Image>
-            </TouchableOpacity>
-        </View>
-    );
-};
-
 const ExploreScreen = () => {
-    const [newsletterData, setNewsletterData]= useState([{title:"Waiting for Data, Please Hit Refresh ..."}]);
-
-    const getAllNewsLetter = async() => {
-        const promise = await fetch(Config.URL+"/api/newsletter/get_newsletters");
-        let data = await promise.json();
-        setNewsletterData(data);
-    };
-
-    useEffect(()=>{
-        getAllNewsLetter();
-    });
 
     return (
         <View style={styles.container}>
@@ -52,15 +24,8 @@ const ExploreScreen = () => {
                     </TouchableOpacity>
                 </View>
             </View>
-            <View style={styles.newsLetter}>
-                <FlatList
-                data={newsletterData}
-                renderItem={({item}) => (
-                    <NewsItem title={item.title} body={item.body} author={item.author}></NewsItem>
-                )}
-                >
-                </FlatList>
-            </View>
+
+            <BlogDisplay></BlogDisplay>
 
             <View style={styles.footer}>
                 <TouchableOpacity
@@ -95,31 +60,6 @@ const styles = StyleSheet.create({
         marginBottom: 0,
         width:'50%',
         height:'100%',
-    },
-    newsLetter: {
-        flex:8,
-        maxHeight:'70vh',
-        backgroundColor:'#FFFFFF',
-		justifyContent: "flex-start",
-        borderColor:"#C4C4C4"
-    },
-    newsItem:{
-        backgroundColor:'#E5E5E550',
-        borderWidth:'1px',
-        borderColor:"#C4C4C470",
-        height:'183px',
-        paddingLeft:'19px',
-        paddingTop:'10px'
-    },
-    newsHeader:{
-        flex:1,
-        justifyContent:'flex-start',
-        flexDirection:'row',
-    },
-    newsTitle:{
-        flex:2,
-        width:'90%',
-        paddingTop:'10px',
     },
     header: {
         flex:2.5,
