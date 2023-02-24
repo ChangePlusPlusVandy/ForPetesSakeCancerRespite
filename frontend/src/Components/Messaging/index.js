6c7470ca7bcfaebbe3f47661eaca9e9171ad70f8
agarg <agarg.usa@gmail.com>
2023-02-23 21:02:42 -0600

    added authentication to socketio

--- frontend/src/Components/Messaging/index.js
+++ frontend/src/Components/Messaging/index.js
@@ -1,63 +1,52 @@
 import React, { Component, useEffect } from "react";
-import {
- View,
- FlatList,
- Text
-} from "react-native";
+import { View, FlatList, Text, StyleSheet } from "react-native";
 import Message from "./Message";
-import MessagesPanel from './MessagesPanel';
+import MessagesPanel from "./MessagesPanel";
 import Groupchat from "./Groupchat";
 import { useNavigation, Link } from "@react-navigation/native";
 import CONFIG from "../../Config";
 
-
 class ChatApp extends Component {
- constructor(props) {
-   super(props);
-   this.state = {
-     groupchats: []
-   };
- }
+	constructor(props) {
+		super(props);
+		this.state = {
+			groupchats: [],
+		};
+	}
 
+	componentDidMount() {
+		//loading channels
+		this.getGroupchats();
+	}
 
- componentDidMount() {
-   //loading channels
-   this.getGroupchats();
- }
+	getGroupchats = async () => {
+		//api request to get all the users
+		fetch(CONFIG.URL + "/get_groupchats").then(async (response) => {
+			let data = await response.json();
+			this.setState({ groupchats: data });
+		});
+	};
 
-
- getGroupchats = async () => {
-   //api request to get all the users
-   fetch(CONFIG.URL + "/get_groupchats").then(async response => {
-       let data = await response.json();
-       this.setState({ groupchats: data });
-   })
-
- }
-
-
- render() {
-
-
-   //navigate to messaging panel
-   return (
-       <View>
-           <View>
-         <FlatList
-           data={this.state.groupchats}
-           renderItem={({ item }) =>
-             <Link to={{screen: "Chat", params: {item}}}>
-             <Groupchat item={item}/>
-             </Link>}
-         />
-       </View>
-       </View>
-   );
- }
+	render() {
+		//navigate to messaging panel
+		return (
+			<View>
+				<View>
+					{/* <MessagesPanel /> */}
+					<FlatList
+						data={this.state.groupchats}
+						renderItem={({ item }) => (
+							<Link to={{ screen: "Chat", params: { item } }}>
+								<Groupchat item={item} />
+							</Link>
+						)}
+					/>
+				</View>
+			</View>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
 
 export default ChatApp;
