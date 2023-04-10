ee648ad2fa755a80d9ec8d365c657dc417d361bd
agarg <agarg.usa@gmail.com>
2023-04-09 22:18:56 -0500

    fixed image api, commented out imagecarasouel

--- backend/src/routes/api/images/create_image.ts
+++ backend/src/routes/api/images/create_image.ts
@@ -1,7 +1,6 @@
-
-const express = require('express');
-const { Storage } = require('@google-cloud/storage');
-const Multer = require('multer');
+const express = require("express");
+const { Storage } = require("@google-cloud/storage");
+const Multer = require("multer");
 import CONFIG from "../../../Config";
 
 const storage = new Storage({ credentials: CONFIG.firebaseCert });
@@ -9,52 +8,55 @@ const bucket = storage.bucket("for-petes-sake-cancer-respite.appspot.com");
 const router = express.Router();
 
 const multer = Multer({
-  storage: Multer.memoryStorage(),
-  limits: {
-    fileSize: 20 * 1024 * 1024 // no larger than 20mb
-  }
+	storage: Multer.memoryStorage(),
+	limits: {
+		fileSize: 8 * 1024 * 1024, // no larger than 8mb
+	},
 });
 
-router.post('/create_image', multer.single('file'), (req, res) => {
-  let file = req.file;
-  if (file) {
-    uploadImageToStorage(file).then((success) => {
-      res.status(200).send({
-        status: 'success'
-      });
-    }).catch((error) => {
-      console.error(error);
-    });
-  }
+router.post("/create_image", multer.single("file"), (req, res) => {
+	let file = req.file;
+	if (file) {
+		uploadImageToStorage(file)
+			.then((url: any) => {
+				res.status(200).json({
+					status: "success",
+					url,
+				});
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
 });
 
 const uploadImageToStorage = (file) => {
-  return new Promise((resolve, reject) => {
-    if (!file) {
-      reject('No image file');
-    }
-    let newFileName = `${file.originalname}_${Date.now()}`;
+	return new Promise((resolve, reject) => {
+		if (!file) {
+			reject("No image file");
+		}
+		let newFileName = `${file.originalname}_${Date.now()}`;
 
-    let fileUpload = bucket.file(newFileName);
+		let fileUpload = bucket.file(newFileName);
 
-    const blobStream = fileUpload.createWriteStream({
-      metadata: {
-        contentType: file.mimetype
-      }
-    });
+		const blobStream = fileUpload.createWriteStream({
+			metadata: {
+				contentType: file.mimetype,
+			},
+		});
 
-    blobStream.on('error', (error) => {
-      reject('ERROR: Unable to Upload File: ' + error.message);
-    });
+		blobStream.on("error", (error) => {
+			reject("ERROR: Unable to Upload File: " + error.message);
+		});
 
-    blobStream.on('finish', () => {
-      // The public URL can be used to directly access the file via HTTP.
-      const url = `https://storage.googleapis.com/for-petes-sake-cancer-respite.appspot.com/${fileUpload.name}`;
-      resolve(url);
-    });
+		blobStream.on("finish", () => {
+			// The public URL can be used to directly access the file via HTTP.
+			const url = `https://firebasestorage.googleapis.com/v0/b/for-petes-sake-cancer-respite.appspot.com/o/${fileUpload.name}?alt=media`;
+			resolve(url);
+		});
 
-    blobStream.end(file.buffer);
-  });
-}
+		blobStream.end(file.buffer);
+	});
+};
 
 export default router;
--- backend/src/routes/api/images/index.ts
+++ backend/src/routes/api/images/index.ts
@@ -1,11 +1,6 @@
 import express from "express";
-import VerifyToken from "../../../middlewares/VerifyToken";
 import create_image from "./create_image";
 const router = express.Router();
-
-router.get("/self", VerifyToken, (req, res) => {
-	res.json((req as any).user);
-});
 
 router.use(create_image);
 
