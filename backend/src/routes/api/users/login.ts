1ce71025f4795360a6e7b832df857ba9e0df3898
Syed Ali <syedansabali786@gmail.com>
2023-04-30 14:18:31 -0500

    sample

--- backend/src/routes/api/users/login.ts
+++ backend/src/routes/api/users/login.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response, NextFunction } from "express";
+import fpscr_account from "../../../Config";
 import {
 	auth,
 	getTokenFromReq,
 	getFromUserTokenAndAddIfNotFound,
 } from "../../../firebase";
+import { User } from "../../../models/User";
 const router = express.Router();
 
 // add new user data to the mongoDB database
@@ -12,6 +14,28 @@ router.post(
 	async (req: Request, res: Response, next: NextFunction) => {
 		try {
 			let user = await getFromUserTokenAndAddIfNotFound(getTokenFromReq(req));
+			// follow FPSCR account
+			if (fpscr_account) {
+				const FPSCR = await User.findById(fpscr_account);
+				// add this id to everyone's else following array
+				
+				if (FPSCR) {
+					// check FPSCR it exists
+					if (user.following.indexOf(FPSCR._id) !== -1) {
+						await User.findByIdAndUpdate(
+							{ _id: user._id },
+							{ $push: { following: FPSCR._id } }
+						);
+					}
+					if (FPSCR.follower.indexOf(user._id) !== -1) {
+						await User.findByIdAndUpdate(
+							{ _id: FPSCR._id },
+							{ $push: { follower: user._id } }
+						);
+					}
+				}
+			}
+
 			return res.json(user);
 		} catch (e) {
 			console.error(e);
