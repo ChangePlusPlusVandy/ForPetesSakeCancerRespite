8cd205729491e15c72e51799af731f3d1a1d1ece
Charlotte Barnes <charbarnz@gmail.com>
2023-04-30 13:46:34 -0500

    Added Pfps to comments, fixed explore/following reload glitch

--- backend/src/routes/api/users/get_profile_picture.ts
+++ backend/src/routes/api/users/get_profile_picture.ts
@@ -3,25 +3,31 @@ import { User } from "../../../models/User";
 const router = express.Router();
 
 router.get("/profile_picture", async (req, res) => {
+  try {
     let user = req.query["id"];
     let mongoUser = await User.findById(user);
     let url = "";
 
     //check if the mongoDB user id exists
     if (!mongoUser) {
-        res.status(400).json({ message: "User Not Found" });
-        return;
-    }
-    else if(!mongoUser.profile_picture || mongoUser.profile_picture == ""){
-        url = "https://firebasestorage.googleapis.com/v0/b/for-petes-sake-cancer-respite.appspot.com/o/defaultProfile.png?alt=media&token=2e4f8b9e-05d6-4c6a-a0fc-0b2c049b5f5d";
-    }
-    else {
-        url = mongoUser.profile_picture;
+      res.status(400).json({ message: "User Not Found" });
+      return;
+    } else if (!mongoUser.profile_picture || mongoUser.profile_picture == "") {
+      url =
+        "https://firebasestorage.googleapis.com/v0/b/for-petes-sake-cancer-respite.appspot.com/o/defaultProfile.png?alt=media&token=2e4f8b9e-05d6-4c6a-a0fc-0b2c049b5f5d";
+    } else {
+      url = mongoUser.profile_picture;
     }
 
     // serve the image at url
-    res.writeHead(302, {location: url});
+    res.writeHead(302, { location: url });
     res.end();
+  } catch (e) {
+    let url =
+      "https://firebasestorage.googleapis.com/v0/b/for-petes-sake-cancer-respite.appspot.com/o/defaultProfile.png?alt=media&token=2e4f8b9e-05d6-4c6a-a0fc-0b2c049b5f5d";
+    res.writeHead(302, { location: url });
+    res.end();
+  }
 });
 
 export default router;
